327d122721f1562982a08f3651404d1dd651ea31
Cyrille Nofficial <cynoffic@cyrilix.fr>
2022-01-18 19:29:30 +0100

    Remove build for arm/v7

--- build-docker.sh
+++ build-docker.sh
@@ -3,9 +3,10 @@
 IMAGE_NAME=robocar-oak-camera
 TAG=$(git describe)
 FULL_IMAGE_NAME=docker.io/cyrilix/${IMAGE_NAME}:${TAG}
+PLATFORM="linux/amd64,linux/arm64"
+#PLATFORM="linux/amd64,linux/arm64,linux/arm/v7"
 
-
-podman build . --platform linux/amd64,linux/arm64,linux/arm/v7 --manifest "${IMAGE_NAME}:${TAG}"
+podman build . --platform "${PLATFORM}" --manifest "${IMAGE_NAME}:${TAG}"
 podman manifest push --format v2s2 "localhost/${IMAGE_NAME}:${TAG}" "docker://${FULL_IMAGE_NAME}"
 
 printf "\nImage %s published" "docker://${FULL_IMAGE_NAME}"
