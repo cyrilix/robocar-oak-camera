c2875df304741e5ff5c7d337c7073ddb05f95cc0
Cyrille Nofficial <cynoffic@cyrilix.fr>
2022-10-20 17:17:02 +0200

    docker: update registry to push

--- build-docker.sh
+++ build-docker.sh
@@ -2,11 +2,11 @@
 
 IMAGE_NAME=robocar-oak-camera
 TAG=$(git describe)
-FULL_IMAGE_NAME=docker.io/cyrilix/${IMAGE_NAME}:${TAG}
+FULL_IMAGE_NAME=git.cyrilix.bzh/robocars/${IMAGE_NAME}:${TAG}
 PLATFORM="linux/amd64,linux/arm64"
 #PLATFORM="linux/amd64,linux/arm64,linux/arm/v7"
 
 podman build . --platform "${PLATFORM}" --manifest "${IMAGE_NAME}:${TAG}"
-podman manifest push --format v2s2 "localhost/${IMAGE_NAME}:${TAG}" "docker://${FULL_IMAGE_NAME}"
+podman manifest push --all --format v2s2 "localhost/${IMAGE_NAME}:${TAG}" "docker://${FULL_IMAGE_NAME}"
 
 printf "\nImage %s published" "docker://${FULL_IMAGE_NAME}"
